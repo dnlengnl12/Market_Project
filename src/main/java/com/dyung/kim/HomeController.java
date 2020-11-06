d22c220fe04ee721093746055d7098e261170d92
LAPTOP-44EIL2TI\dnlen <dnlengnl@naver.com>
2020-11-06 15:06:40 +0900

    private information

--- src/main/java/com/dyung/kim/HomeController.java
+++ src/main/java/com/dyung/kim/HomeController.java
@@ -1,22 +1,15 @@
 package com.dyung.kim;
 
-import java.io.UnsupportedEncodingException;
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-import javax.servlet.http.HttpSession;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.dyung.kim.service.AccountService;
-import com.dyung.kim.vo.accountVO;
+import com.dyung.kim.service.ItemService;
 
 
 /**
@@ -24,30 +17,18 @@ import com.dyung.kim.vo.accountVO;
  */
 @Controller
 public class HomeController {
-	
-	@Autowired
-	private AccountService service;
-	
-	private HttpSession session;
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
+   
+   @Autowired
+   private ItemService is;
+   
+   @RequestMapping(value = "/", method = RequestMethod.GET)
+   public String home(Model model) {
+      ArrayList<HashMap<Object,Object>> list = is.selectItemAll();
+      
+      model.addAttribute("list", list);
+      
+      return "home";
+   }
 
-	
+   
 }
